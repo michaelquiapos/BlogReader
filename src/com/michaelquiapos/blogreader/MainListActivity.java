1e138c54bd3619171f04f3ccc00dc3643bd9b579
michaelquiapos <info@michaelquiapos.com>
2013-04-15 00:39:18 +0800

    HOME: Parsing JSON
    Parsing Data Returned in JSON Format

--- src/com/michaelquiapos/blogreader/MainListActivity.java
+++ src/com/michaelquiapos/blogreader/MainListActivity.java
@@ -1,30 +1,63 @@
 package com.michaelquiapos.blogreader;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import android.app.ListActivity;
-import android.content.res.Resources;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
-import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 
 public class MainListActivity extends ListActivity {
 	
 	// We generally want to use protected for member variables unless we have specific reason to make them public
 	// DATA
-	protected String[] mAndroidNames;
+	protected String[] mBlogPostTitles;
+	public static final int NUMBER_OF_POSTS	= 20;
+	public static final String TAG = MainListActivity.class.getSimpleName();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_list);
-
-		Resources resources = getResources();
-		mAndroidNames = resources.getStringArray(R.array.android_names);
 		
-		// Adaptor
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mAndroidNames);
-		setListAdapter(adapter);
+		if(isNetworkAvailable()) {
+		// Alert if network is unavailable
+		GetBlogPostTask getBlogPostTask = new GetBlogPostTask();
+		getBlogPostTask.execute();
+		
+		}
+		//Toast.makeText(this, getString(R.String.no_items), Toast.LENGTH_LONG).show();
+	}
 
+	private boolean isNetworkAvailable() {
+		ConnectivityManager manager = (ConnectivityManager) 
+				getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = manager.getActiveNetworkInfo(); 
+		
+		boolean isAvailable = false;
+		
+		if (networkInfo != null && networkInfo.isConnected()) {
+			isAvailable = true;
+		} else {
+			Toast.makeText(this, "Network is unvailable!", Toast.LENGTH_LONG).show();
+		}
+		
+		return isAvailable;
 	}
 
 	@Override
@@ -32,6 +65,58 @@ public class MainListActivity extends ListActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main_list, menu);
 		return true;
+	}
+	
+	private class GetBlogPostTask extends AsyncTask<Object, Void, String> {
+		
+		int responseCode = -1;
+		
+		@Override
+		protected String doInBackground(Object... params) {
+			try {
+				URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
+				HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+				connection.connect();
+				
+				if (responseCode == HttpURLConnection.HTTP_OK) {
+					InputStream inputStream = connection.getInputStream();
+					Reader reader = new InputStreamReader(inputStream);
+					int contentLength = connection.getContentLength();
+					char[] charArray = new char[contentLength];
+					reader.read(charArray);
+					String responseData = new String(charArray);
+					//Log.v(TAG, responseData);
+					
+					JSONObject jsonResponse = new JSONObject(responseData);
+					String status = jsonResponse.getString("status");
+					Log.v(TAG, status);
+					
+					JSONArray jsonPosts = jsonResponse.getJSONArray(responseData);
+					for (int i=0; i < jsonPosts.length(); i++) {
+						JSONObject jsonPost = jsonPosts.getJSONObject(i);
+						String title = jsonPost.getString("title");
+						Log.v(TAG, "Post " + i + ": " + title);
+					}
+							
+				}
+				else {
+					Log.i(TAG, "Unsuccessful HTTP Response Code: " + responseCode);
+				}
+				
+				Log.i(TAG, "Code: " + responseCode);
+			}
+			catch (MalformedURLException e) {
+				Log.e(TAG, "Exception caught: ", e);
+			} 
+			catch (IOException e){
+				Log.e(TAG, "Exception caught: ", e);
+			}
+			catch (Exception e){
+				Log.e(TAG, "Exception caught: ", e);
+			}
+			return "Code: " + responseCode;
+		}
+		
 	}
 
 }

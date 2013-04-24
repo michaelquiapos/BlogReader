620da9038df0b979f4d6bf9b4815d6a8ebbf5f7d
michaelquiapos <info@michaelquiapos.com>
2013-04-24 19:43:51 +0800

    HOME: Android Development
    Build a Blog Reader Android App> Adapting Data for Display in a List >
    Cleaning Up Our Presentation

--- src/com/michaelquiapos/blogreader/MainListActivity.java
+++ src/com/michaelquiapos/blogreader/MainListActivity.java
@@ -9,16 +9,20 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 
@@ -29,6 +33,7 @@ public class MainListActivity extends ListActivity {
 	protected String[] mBlogPostTitles;
 	public static final int NUMBER_OF_POSTS	= 20;
 	public static final String TAG = MainListActivity.class.getSimpleName();
+	protected JSONObject mBlogData;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -66,13 +71,49 @@ public class MainListActivity extends ListActivity {
 		getMenuInflater().inflate(R.menu.main_list, menu);
 		return true;
 	}
+		
+	public void updateList() {
+		if (mBlogData == null) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle(getString(R.string.error_title));
+			builder.setMessage(getString(R.string.error_message));
+			builder.setPositiveButton(android.R.string.ok, null);
+			AlertDialog dialog = builder.create();
+			dialog.show();
+		}
+		else {
+			try {
+				// Log.d(TAG, mBlogData.toString(2));
+				JSONArray jsonPosts = mBlogData.getJSONArray("posts");
+				mBlogPostTitles = new String[jsonPosts.length()];
+				
+				for (int i = 0; i < jsonPosts.length(); i++){
+					
+					JSONObject post = jsonPosts.getJSONObject(i);
+					String title = post.getString("title");
+					title = Html.fromHtml(title).toString();
+					mBlogPostTitles[i] = title;
+					
+				}
+				
+				ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, 
+						android.R.layout.simple_list_item_1, mBlogPostTitles);
+				setListAdapter(adapter);
+				
+			} 
+			catch (JSONException e) {
+				Log.e(TAG, "Exception caught", e);
+			}
+		}
+	}
 	
-	private class GetBlogPostTask extends AsyncTask<Object, Void, String> {
+	private class GetBlogPostTask extends AsyncTask<Object, Void, JSONObject> {
 		
 		int responseCode = -1;
+		JSONObject jsonResponse = null;
 		
 		@Override
-		protected String doInBackground(Object... params) {
+		protected JSONObject doInBackground(Object... params) {
 			try {
 				URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
 				HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
@@ -87,18 +128,7 @@ public class MainListActivity extends ListActivity {
 					String responseData = new String(charArray);
 					//Log.v(TAG, responseData);
 					
-					JSONObject jsonResponse = new JSONObject(responseData);
-					String status = jsonResponse.getString("status");
-					Log.v(TAG, status);
-					
-					// loop through
-					JSONArray jsonPosts = jsonResponse.getJSONArray(responseData);
-					for (int i=0; i < jsonPosts.length(); i++) {
-						JSONObject jsonPost = jsonPosts.getJSONObject(i);
-						String title = jsonPost.getString("title");
-						Log.v(TAG, "Post " + i + ": " + title);
-					}
-							
+					jsonResponse = new JSONObject(responseData);							
 				}
 				else {
 					Log.i(TAG, "Unsuccessful HTTP Response Code: " + responseCode);
@@ -115,9 +145,13 @@ public class MainListActivity extends ListActivity {
 			catch (Exception e){
 				Log.e(TAG, "Exception caught: ", e);
 			}
-			return "Code: " + responseCode;
+			return jsonResponse;
 		}
 		
+		@Override
+		protected void onPostExecute(JSONObject result) {
+			mBlogData = result;
+			updateList();
+		}
 	}
-
 }

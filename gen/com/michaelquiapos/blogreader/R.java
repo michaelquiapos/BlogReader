f8932508303229e6698d365d34c6486ae0dc00a0
Michael Quiapos <info@michaelquiapos.com>
2013-05-06 15:52:14 +0800

    OFFICE: Adding a Different Icon for Older Menus
    Using Intents to Display and Share Posts > Adding a Different Icon for
    Older Menus

--- gen/com/michaelquiapos/blogreader/R.java
+++ gen/com/michaelquiapos/blogreader/R.java
@@ -29,7 +29,6 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
         public static final int action_share=0x7f090002;
         public static final int progressBar1=0x7f090001;
         public static final int webView1=0x7f090000;
@@ -40,7 +39,6 @@ public final class R {
     }
     public static final class menu {
         public static final int blog_web_view=0x7f080000;
-        public static final int main_list=0x7f080001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
